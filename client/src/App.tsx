5ec0580431fdd380a444f27248c74fd17628ff1f
Awais4813 <ranaawais48131@outlook.com>
2024-12-24 22:56:59 +0500

    basic template

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-dark'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
