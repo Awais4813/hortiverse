22305ad1a2d3d52e602a3eb230afd66e7de93327
Awais4813 <ranaawais48131@outlook.com>
2024-12-26 07:20:13 +0500

    FE_HomePage: (Section 1 completed)

--- client/src/constants/colors.ts
+++ client/src/constants/colors.ts
@@ -1,2 +1,3 @@
 export const navBarBG = "#15301D";
 export const primaryText = "#00AE35";
+export const secondaryText = "#00AD34";
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
-
+import "../index.css";
+import "../styles/homepage.css";
+import { secondaryText } from "../constants/colors";
 export default function HomePage() {
   const [sectionHeight, setSectionHeight] = useState(0);
   useEffect(() => {
     const navigationBar = document.getElementById("navigationBar");
-    // const homePageSection1 = document.getElementById("homePageSection1");
     if (navigationBar) {
       const newSectionHeight =
         window.innerHeight - navigationBar.getBoundingClientRect().height;
       setSectionHeight(newSectionHeight);
     }
-}, []);
+  }, []);
   return (
     <>
       <section
@@ -34,15 +35,73 @@ export default function HomePage() {
           }}
         ></video>
         <div
+          id="homepageSection1-contentContainer"
           style={{
             position: "absolute",
             width: "100%",
             height: `${sectionHeight}px`,
             backgroundColor: "rgba(0,0,0,0.0)",
             top: 0,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+            padding: "1.5rem",
           }}
         >
-            <h1 style={{color:"white",fontSize:"15rem"}}>Hello<br/>World</h1>
+          <div id="homepageSection1-leftPane">
+            <div
+              id="homepageSection1-leftPane-heading"
+              style={{
+                color: secondaryText,
+                fontFamily: "integralCfBold",
+                fontSize: "3rem",
+                width: "400px",
+                marginBottom: "15px",
+                lineHeight: "35px",
+              }}
+            >
+              Nursery <span style={{ fontSize: "2.6rem" }}>and</span>
+              <br /> Landscapes
+            </div>
+            <div
+              style={{
+                color: "white",
+                fontFamily: "Raleway-Bold",
+                textShadow: "0 7px 7px rgba(0, 0, 0, 0.5)",
+                marginBottom: "2.5rem",
+              }}
+            >
+              Browse through our diverse range of plants, garden accessories and
+              landscape services.
+            </div>
+            <div
+              id="primaryBtn"
+              style={{
+                width: "210px",
+                height: "45px",
+              }}
+            >
+              Shop Now
+            </div>
+            <div id="homepageSection1-leftPane-reviewContainer">
+              <div className="homepageSection1-leftPane-reviewPane" style={{paddingLeft:0}}>
+                <div className="homepageSection1-leftPane-reviewPaneHeading">200+</div>
+                <div className="homepageSection1-leftPane-reviewPaneSubHeading">International Customers</div>
+              </div>
+              <div className="homepageSection1-leftPane-reviewPane">
+                <div className="homepageSection1-leftPane-reviewPaneHeading">2,000+</div>
+                <div className="homepageSection1-leftPane-reviewPaneSubHeading">High Quality Products</div>
+              </div>
+              <div className="homepageSection1-leftPane-reviewPane" style={{borderRight:"none"}}>
+                <div className="homepageSection1-leftPane-reviewPaneHeading">30,000+</div>
+                <div className="homepageSection1-leftPane-reviewPaneSubHeading">Happy Customers</div>
+              </div>
+            </div>
+          </div>
+          <div id="homepageSection1-RightPane">
+            <img src="/assets/icon with shadow.png" alt="" style={{ width: "100%" }} />
+          </div>
         </div>
       </section>
     </>
